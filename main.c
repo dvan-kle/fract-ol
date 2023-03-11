231eab7f40c2aaa52faef863124c760fd930525b
Daniel <115094794+dvan-kle@users.noreply.github.com>
2023-03-11 21:56:47 +0100

    11/03

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dvan-kle <dvan-kle@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/09 17:47:39 by dvan-kle      #+#    #+#                 */
-/*   Updated: 2023/03/09 18:12:17 by dvan-kle      ########   odam.nl         */
+/*   Updated: 2023/03/11 21:33:40 by dvan-kle      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,59 @@
 
 #define WIDTH 800
 #define HEIGHT 600
-#define MAX_ITERATIONS 5000
+#define MAX_ITERATIONS 500
 
 static mlx_image_t* image;
 
 typedef struct s_complex {
-    double real;
-    double imag;
-} t_complex;
+	double	real;
+	double	imag;
 
-int mandelbrot(t_complex c) 
+}	t_complex;
+
+int	mandelbrot(t_complex c)
 {
-    t_complex z;
-    z.real = 0;
-    z.imag = 0;
-    int iterations = 0;
-    while (iterations < MAX_ITERATIONS && (z.real * z.real + z.imag * z.imag) < 4) {
-        t_complex tmp;
-        tmp.real = z.real * z.real - z.imag * z.imag + c.real;
-        tmp.imag = 2 * z.real * z.imag + c.imag;
-        z = tmp;
-        iterations++;
-    }
-    return iterations;
+	t_complex	z;
+	t_complex	tmp;
+	int			iterations;
+
+	z.real = 0;
+	z.imag = 0;
+	iterations = 0;
+	while (iterations < MAX_ITERATIONS
+		&& (z.real * z.real + z.imag * z.imag) < 4)
+	{
+		tmp.real = z.real * z.real - z.imag * z.imag + c.real;
+		tmp.imag = 2 * z.real * z.imag + c.imag;
+		z = tmp;
+		iterations++;
+	}
+	return (iterations);
 }
 
-void ft_hook(void* param)
+int	mandel_iter(int x, int y, t_complex c)
 {
-	mlx_t* mlx = param;
+	double		x_min;
+	double		xy_max;
+	double		y_min;
+	double		x_step;
+	double		y_step;
 
+	x_min = -2.0;
+	xy_max = 1.0;
+	y_min = -1.0;
+	x_step = (xy_max - x_min) / WIDTH;
+	y_step = (xy_max - y_min) / HEIGHT;
+	c.real = x_min + x * x_step;
+	c.imag = xy_max - y * y_step;
+	return (mandelbrot(c));
+}
+
+void	ft_hook(void *param)
+{
+	mlx_t	*mlx;
+
+	mlx = param;
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
@@ -57,12 +81,12 @@ void ft_hook(void* param)
 		image->instances[0].x += 100;
 }
 
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+int32_t	ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
 {
-    return (r << 24 | g << 16 | b << 8 | a);
+	return (r << 24 | g << 16 | b << 8 | a);
 }
 
-int ft_coloring(iterations)
+int	ft_coloring(int iterations)
 {
 	int	color;
 
@@ -78,88 +102,59 @@ int ft_coloring(iterations)
 		color = ft_pixel(63, 0, 0, 255);
 	if (iterations == MAX_ITERATIONS)
 		color = ft_pixel(0, 0, 0, 255);
-
-	return color;
-
+	return (color);
 }
 
-void ft_mandel(void* param)
+void	ft_mandel(void *param)
 {
-	mlx_t* mlx = param;
+	t_complex	c;
+	int			y;
+	int			x;
+	int			color;
 
-	mlx = NULL;
-	double x_min = -2.0, x_max = 1.0;
-    double y_min = -1.0, y_max = 1.0;
-    double x_step = (x_max - x_min) / WIDTH;
-    double y_step = (y_max - y_min) / HEIGHT;
-
-    int y = 0;
-    while (y < HEIGHT) {
-        int x = 0;
-        while (x < WIDTH) {
-            t_complex c;
-            c.real = x_min + x * x_step;
-            c.imag = y_max - y * y_step;
-			int color = mandelbrot(c);
-            if (color == MAX_ITERATIONS) 
-			{
-				color = ft_pixel(0, 0, 0, 255);
-                mlx_put_pixel(image, x, y, color);
-            } 
-			else 
-			{
-                int r = 0, g = 122, b = 255;
-                if (color < 256) {
-                    r = color;
-                } else if (color < 512) {
-                    r = 255;
-                    g = color - 255;
-                } else if (color < 768) 
-				{
-                    r = 255;
-                    g = 255;
-                    b = color - 512;
-                }
-				else 
-				{
-                    r = 255 - (color - 768);
-                    g = 255 - (color - 768);
-                    b = 255 - (color - 768);
-                }
-                int color_code = (r << 16) + (g << 8) + b;
-                mlx_put_pixel(image, x, y, color_code);
-			}
-            x++;
-        }
-        y++;
-    }
+	y = 0;
+	c.real = 0;
+	c.imag = 0;
+	while (y < HEIGHT)
+	{
+		x = 0;
+		while (x < WIDTH)
+		{
+			color = mandel_iter(x, y, c);
+			if (color == MAX_ITERATIONS)
+				mlx_put_pixel(image, x, y, ft_pixel(0, 0, 0, 255));
+			else
+				mlx_put_pixel(image, x, y, ft_coloring(color));
+			x++;
+		}
+		y++;
+	}
 }
 
-int main() {
-   
+int	main(void)
+{
 	mlx_t* mlx;
 
-   if (!(mlx = mlx_init(WIDTH, HEIGHT, "Fract-ol", true)))
+	if (!(mlx = mlx_init(WIDTH, HEIGHT, "Fract-ol", true)))
 	{
 		printf("%s", mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	if (!(image = mlx_new_image(mlx, 800, 600)))
 	{
 		mlx_close_window(mlx);
 		printf("%s", mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
 	{
 		mlx_close_window(mlx);
 		printf("%s", mlx_strerror(mlx_errno));
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
-
-    mlx_loop_hook(mlx, ft_mandel, mlx);
+	mlx_loop_hook(mlx, ft_mandel, mlx);
 	mlx_loop_hook(mlx, ft_hook, mlx);
-
+	mlx_put_string(mlx, "MANDELBROT", WIDTH / 2, HEIGHT / 2);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
